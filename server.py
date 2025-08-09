9e991487f8a7f3e08e728dd07ba49dc88118b757
Jnnamchi <john.nnamchi@mail.mcgill.ca>
2025-08-08 19:07:57 -0700

    Added HTML route

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI, Query
 from request import fetch_species_data
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
 
 app = FastAPI()
+
+BASE_DIR = Path(__file__).resolve().parent
 
 @app.get("/hello")
 def read_hello():
@@ -11,3 +16,8 @@ def read_hello():
 def get_species(search: str = Query(...)):
     print("I got: " + search)
     return { "data": fetch_species_data(search)}
+
+@app.get("/", response_class=FileResponse)
+def return_site():
+    # Serve the HTML file directly
+    return FileResponse(BASE_DIR / "templates" / "index.html", media_type="text/html")
